eeaa2725ba4c5d3bda80a25cf640d3e4344a683f
Dusan <dusanvulic11@gmail.com>
2023-01-10 14:09:08 +0100

    adding styles after accessing to rote params

--- src/components/CareerDetail.js
+++ src/components/CareerDetail.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useParams, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 const CareerDetail = () => {
-  const { id } = useParams();
-
   const career = useLoaderData();
   return (
     <div className="carrer-details">
       <h3>CareerDetail for {career.title}</h3>
-      <p>starting salary {career.salary}</p>
-      <p>location :{career.location}</p>
-      <div className="details">lorem10</div>
+      <p>starting salary : {career.salary}</p>
+      <p>location : {career.location}</p>
+      <div className="details">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, sint.
+      </div>
     </div>
   );
 };
